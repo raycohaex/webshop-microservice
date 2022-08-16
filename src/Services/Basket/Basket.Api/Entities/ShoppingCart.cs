c48e1923253e8ff8a9f43b9bc6394b778812443e
Rayco Haex <raycoh1299@gmail.com>
2022-08-16 22:07:03 +0200

    Added entities

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        private string UserName { get; set; }
+        private List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
