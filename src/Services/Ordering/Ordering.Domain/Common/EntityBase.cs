b5c2a5588226a11be6a999cc029c593c74f45ade
Rayco Haex <raycoh1299@gmail.com>
2022-08-26 22:28:09 +0200

    Added essential classes + ordering class to domain layer

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+        public int UpdatedOn { get; set; }
+    }
+}
