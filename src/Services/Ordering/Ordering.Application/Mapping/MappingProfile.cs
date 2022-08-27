4511079021ab41f12cb28b19477b2d0a90e44576
Rayco Haex <raycoh1299@gmail.com>
2022-08-27 18:36:32 +0200

    Implemented update CQRS functionality for orders

--- src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mapping/MappingProfile.cs
@@ -1,12 +1,8 @@
 ﻿using AutoMapper;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Application.Mapping
 {
@@ -16,6 +12,7 @@ namespace Ordering.Application.Mapping
         {
             CreateMap<Order, OrdersVm>().ReverseMap();
             CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
 }
