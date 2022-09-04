31a83419b3c7c98434f1d93fcd67509e83e007cc
Rayco Haex <raycoh1299@gmail.com>
2022-09-04 18:06:54 +0200

    Added ocelot gateway config

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,6 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("CORSPolicy", builder => builder.AllowAnyMethod().AllowAnyHeader().AllowCredentials().SetIsOriginAllowed((hosts) => true));
+});
+
+builder.Services.AddControllers();
+
+builder.Configuration.AddJsonFile(builder.Configuration.GetValue<string>("Gateway:config"), optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseCors("CORSPolicy");
+app.UseAuthorization();
+app.MapControllers();
+await app.UseOcelot();
 
 app.MapGet("/", () => "Hello World!");
 
